ff035481fd7257cbad79e1523009b3e56100f650
Samjukta <samjuktarlb@gmail.com>
2016-09-02 11:49:59 -0700

    Add files via upload

---
+++ Solution.R
@@ -0,0 +1,42 @@
+
+# This script trains a Random Forest model based on the data,
+# saves a sample submission
+# Author : Samjukta Daz
+# Email Id : samjuktarlb@gmail.com
+# Date oF Coding : 29-08-2016
+
+# library for randomForest tool to include.
+library(randomForest)
+
+set.seed(1)
+# store all data in train
+train <- read.csv("titanic_train.csv", stringsAsFactors=FALSE)
+# store all data in test
+test  <- read.csv("titanic_test.csv",  stringsAsFactors=FALSE)
+
+# function to extract Features 
+extractFeatures <- function(data) {
+  features <- c(
+				"Pclass",
+                "Age",
+                "Sex",
+                "Parch",
+                "SibSp",
+                "Fare",
+                "Embarked")
+  fea <- data[,features]
+  fea$Age[is.na(fea$Age)] <- -1
+  fea$Fare[is.na(fea$Fare)] <- mean(fea$Fare, na.rm=TRUE)
+  fea$Embarked[fea$Embarked==""] = "S"
+  fea$Sex      <- as.factor(fea$Sex)
+  fea$Embarked <- as.factor(fea$Embarked)
+  return(fea)
+}
+# apply randome Forest function to get reference data using Survived variable
+rf <- randomForest(extractFeatures(train), as.factor(train$Survived), ntree=10000, importance=TRUE)
+#make a submission frame 
+submission <- data.frame(PassengerId = test$PassengerId)
+#predict result using reference data for test data
+submission$Survived <- predict(rf, extractFeatures(test))
+# write the predict result to titanic_sample_submission file.
+write.csv(submission, file = "titanic_sample_submission.csv",row.names=FALSE)
